031caad87b2277b45eaf9b1f69968b10ae729ab6
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-17 20:54:43 +0200

    completed ErrorDisplay functionality as Dialog added showError function in utils\general

--- lib/util/general.dart
+++ lib/util/general.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:todays_kanji/model/kanji_model.dart';
+import 'package:todays_kanji/widgets/error_display.dart';
 
 T cast<T>(x) => x is T ? x : null;
 
 int todayTimestamp() {
   var now = DateTime.now().millisecondsSinceEpoch;
   return now - now % 86400000;
+}
+
+void showError(BuildContext context, String text, {void Function() onClose}) {
+  Future.delayed(Duration.zero, () {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return ErrorDisplay(child: Text(text));
+      },
+    ).then((value) => onClose());
+  });
 }
 
 class RubberBandScroll extends ScrollBehavior {
--- lib/widgets/error_display.dart
+++ lib/widgets/error_display.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ErrorDisplay extends StatelessWidget {
-  final Widget header;
   final Widget child;
 
-  ErrorDisplay({this.header, this.child});
+  ErrorDisplay({this.child});
 
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.all(26),
-        child: Card(
-          color: theme.errorColor,
-          child: Column(
-            children: [
-              header,
-              child,
-            ],
-          ),
-        ),
-      ),
+    return AlertDialog(
+      title: Text("An Error Occured"),
+      backgroundColor: theme.errorColor,
+      content: child,
     );
   }
 }

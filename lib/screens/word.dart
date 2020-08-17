9b97f0b8503b109be4672a5aeb8167ea6ff2c55d
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-17 22:58:40 +0200

    Added WordScreen with LargeWordView

--- lib/screens/word.dart
+++ lib/screens/word.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:todays_kanji/data_source/kanji_source.dart';
+import 'package:todays_kanji/model/word_model.dart';
+import 'package:todays_kanji/util/general.dart';
+import 'package:todays_kanji/view/large_word_view.dart';
+import 'package:todays_kanji/widgets/content_loader.dart';
 
 class WordScreen extends StatelessWidget {
   static const ROUTENAME = "/word";
+  final kanjiSource = KanjiSource();
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final String word = cast<String>(ModalRoute.of(context).settings.arguments);
+
+    if (word == null) {
+      showError(
+        context,
+        "The supplied word was null or of invalid type",
+        onClose: () => Navigator.pop(context),
+      );
+      return Container();
+    }
+
+    Future<WordModel> modelFuture = kanjiSource.getWord(word);
+
+    return Scaffold(
+      appBar: AppBar(title: Text("View Kanji $word")),
+      body: SizedBox.expand(
+        child: ContentLoader<WordModel>(
+          future: modelFuture,
+          builder: (context, model) {
+            return LargeWordView(model);
+          },
+        ),
+      ),
+    );
   }
 }
---
+++ lib/view/large_word_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:todays_kanji/model/word_model.dart';
+import 'package:todays_kanji/widgets/annotation.dart';
+import 'package:todays_kanji/widgets/japanese_text.dart';
+import 'package:todays_kanji/widgets/large_view_layout.dart';
+
+class LargeWordView extends StatelessWidget {
+  final WordModel model;
+
+  LargeWordView(this.model);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> annotations = [];
+    if (model.jlpt > 0) annotations.add(JLPTAnnotation(model.jlpt));
+    if (model.common) annotations.add(Annotation("common"));
+    return LargeViewLayout(
+      focus: JapaneseText(model.forms[0].word),
+    );
+  }
+}

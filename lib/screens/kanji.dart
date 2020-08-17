102231725b78b4ffd07d37e824c052165fd1e7d3
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-17 21:09:53 +0200

    Completed KanjiScreen

--- lib/screens/kanji.dart
+++ lib/screens/kanji.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:todays_kanji/data_source/kanji_source.dart';
+import 'package:todays_kanji/model/kanji_model.dart';
+import 'package:todays_kanji/util/general.dart';
+import 'package:todays_kanji/view/large_kanji_view.dart';
+import 'package:todays_kanji/widgets/content_loader.dart';
 
 class KanjiScreen extends StatelessWidget {
+  static const ROUTENAME = "/kanji";
+
+  final kanjiSource = KanjiSource();
+
   @override
   Widget build(BuildContext context) {
     //Maybe add option to use KanjiModel instead of String?
-    final String kanjiSymbol = ModalRoute.of(context).settings.arguments;
+    final String kanjiSymbol =
+        cast<String>(ModalRoute.of(context).settings.arguments);
+
+    if (kanjiSymbol == null) {
+      showError(
+        context,
+        "The supplied kanji symbol was null or of invalid type",
+        onClose: () => Navigator.pop(context),
+      );
+      return Container();
+    }
+
+    Future<KanjiModel> modelFuture = kanjiSource.getKanji(kanjiSymbol);
+
+    return Scaffold(
+      appBar: AppBar(title: Text("View Kanji $kanjiSymbol")),
+      body: ContentLoader<KanjiModel>(
+        future: modelFuture,
+        builder: (context, model) {
+          return LargeKanjiView(model);
+        },
+      ),
+    );
   }
 }

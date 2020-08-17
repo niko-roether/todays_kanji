ca3a52af3ac198aad9dd0a07d2eb18854aae4792
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-17 21:04:24 +0200

    Made large_kanji_view an AppState consumer

--- lib/view/large_kanji_view.dart
+++ lib/view/large_kanji_view.dart
@@ -18,164 +18,166 @@ class LargeKanjiView extends StatelessWidget {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     var appState = Provider.of<AppState>(context);
-    return ListView(
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 18, vertical: 18),
-          child: Stack(
-            alignment: Alignment.topLeft,
-            children: [
-              IconButton(
-                icon: Icon(Icons.refresh),
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: Text("Confirm Reroll"),
-                        content: SingleChildScrollView(
-                          child: Text(
-                            "Are you shure you want to reroll your daily kanji?",
+    return Consumer<AppState>(builder: (context, state, child) {
+      return ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 18, vertical: 18),
+            child: Stack(
+              alignment: Alignment.topLeft,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Confirm Reroll"),
+                          content: SingleChildScrollView(
+                            child: Text(
+                              "Are you shure you want to reroll your daily kanji?",
+                            ),
                           ),
-                        ),
-                        actions: [
-                          FlatButton(
-                            child: Text("REROLL"),
-                            onPressed: () {
-                              //TODO export this to controller
-                              controller.rerollKanjiSymbol();
-                              appState.loadingKanji = true;
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          FlatButton(
-                            child: Text("CANCEL"),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          )
-                        ],
-                      );
-                    },
-                  );
-                },
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Builder(builder: (BuildContext context) {
-                  var annotations = <Widget>[];
-                  if (model.frequency != null) {
-                    annotations.add(Annotation(
-                      "${model.frequency}/2500",
-                    ));
-                  }
-                  if (model.jlpt != null) {
-                    annotations.add(JLPTAnnotation(model.jlpt));
-                  }
-                  return Column(
-                    children: annotations,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                  );
-                }),
-              ),
-              Center(
-                child: Column(
-                  children: [
-                    JapaneseText(
-                      model.character,
-                      style: TextStyle(fontSize: 150),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 50),
-                      child: Text(
-                        model.meaning.join(", "),
-                        style: theme.textTheme.subtitle1,
-                        textAlign: TextAlign.center,
-                      ),
-                    )
-                  ],
+                          actions: [
+                            FlatButton(
+                              child: Text("REROLL"),
+                              onPressed: () {
+                                //TODO export this to controller
+                                controller.rerollKanjiSymbol();
+                                appState.loadingKanji = true;
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            FlatButton(
+                              child: Text("CANCEL"),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            )
+                          ],
+                        );
+                      },
+                    );
+                  },
                 ),
-              ),
-            ],
-          ),
-        ),
-        Builder(builder: (BuildContext context) {
-          List<Widget> readings = [];
-          if (model.kunyomi.length > 0)
-            readings.add(JapaneseText(model.kunyomi.join(", ")));
-          if (model.onyomi.length > 0)
-            readings.add(JapaneseText(model.onyomi.join(", ")));
-          if (readings.length == 0) return Container();
-          return InfoCard(
-            heading: "Pronunciation",
-            contentIndent: 20,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: readings,
-            ),
-          );
-        }),
-        Builder(
-          builder: (BuildContext context) {
-            if (model.strokeOrderGifUrl == null) return Container();
-            return InfoCard(
-              heading: "Stroke Order",
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 26),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Builder(builder: (BuildContext context) {
+                    var annotations = <Widget>[];
+                    if (model.frequency != null) {
+                      annotations.add(Annotation(
+                        "${model.frequency}/2500",
+                      ));
+                    }
+                    if (model.jlpt != null) {
+                      annotations.add(JLPTAnnotation(model.jlpt));
+                    }
+                    return Column(
+                      children: annotations,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                    );
+                  }),
+                ),
+                Center(
+                  child: Column(
                     children: [
-                      Image.network(
-                        model.strokeOrderGifUrl,
-                        frameBuilder:
-                            (context, child, frame, wasSynchronouslyLoaded) {
-                          if (wasSynchronouslyLoaded || frame != null)
-                            return child;
-                          return LoadingIndicator();
-                        },
-                        errorBuilder: (context, error, stackTrace) {
-                          // return Image.network(
-                          //   model.strokeOrderDiagramUrl,
-                          //   frameBuilder: (context, child, frame,
-                          //       wasSynchronouslyLoaded) {
-                          //     if (child != null) return child;
-                          //     return LoadingIndicator();
-                          //   },
-                          //   errorBuilder: (context, error, stackTrace) =>
-                          //       Text("Couldn't get stroke order"),
-                          // );
-                          return Text("Failed to load stroke order gif.");
-                        },
+                      JapaneseText(
+                        model.character,
+                        style: TextStyle(fontSize: 150),
                       ),
-                      // TODO add alternatives
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 50),
+                        child: Text(
+                          model.meaning.join(", "),
+                          style: theme.textTheme.subtitle1,
+                          textAlign: TextAlign.center,
+                        ),
+                      )
                     ],
                   ),
                 ),
-              ),
-            );
-          },
-        ),
-        Builder(
-          builder: (BuildContext context) {
-            var wordList = <Widget>[];
-            if (model.examples == null) return Container();
-            for (var word in model.examples) {
-              wordList.add(WordView(word, partition: false));
-              wordList.add(Divider());
-            }
-            wordList.removeLast();
-            if (wordList.length == 0) return Container();
+              ],
+            ),
+          ),
+          Builder(builder: (BuildContext context) {
+            List<Widget> readings = [];
+            if (model.kunyomi.length > 0)
+              readings.add(JapaneseText(model.kunyomi.join(", ")));
+            if (model.onyomi.length > 0)
+              readings.add(JapaneseText(model.onyomi.join(", ")));
+            if (readings.length == 0) return Container();
             return InfoCard(
-              heading: "Examples",
+              heading: "Pronunciation",
               contentIndent: 20,
               child: Column(
-                children: wordList,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: readings,
               ),
             );
-          },
-        ),
-      ],
-    );
+          }),
+          Builder(
+            builder: (BuildContext context) {
+              if (model.strokeOrderGifUrl == null) return Container();
+              return InfoCard(
+                heading: "Stroke Order",
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 26),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          model.strokeOrderGifUrl,
+                          frameBuilder:
+                              (context, child, frame, wasSynchronouslyLoaded) {
+                            if (wasSynchronouslyLoaded || frame != null)
+                              return child;
+                            return LoadingIndicator();
+                          },
+                          errorBuilder: (context, error, stackTrace) {
+                            // return Image.network(
+                            //   model.strokeOrderDiagramUrl,
+                            //   frameBuilder: (context, child, frame,
+                            //       wasSynchronouslyLoaded) {
+                            //     if (child != null) return child;
+                            //     return LoadingIndicator();
+                            //   },
+                            //   errorBuilder: (context, error, stackTrace) =>
+                            //       Text("Couldn't get stroke order"),
+                            // );
+                            return Text("Failed to load stroke order gif.");
+                          },
+                        ),
+                        // TODO add alternatives
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          Builder(
+            builder: (BuildContext context) {
+              var wordList = <Widget>[];
+              if (model.examples == null) return Container();
+              for (var word in model.examples) {
+                wordList.add(WordView(word, partition: false));
+                wordList.add(Divider());
+              }
+              wordList.removeLast();
+              if (wordList.length == 0) return Container();
+              return InfoCard(
+                heading: "Examples",
+                contentIndent: 20,
+                child: Column(
+                  children: wordList,
+                ),
+              );
+            },
+          ),
+        ],
+      );
+    });
   }
 }

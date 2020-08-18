c1582e660dc2004d984c11c3aaada1a3a48a5f61
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-18 21:55:06 +0200

    completed

--- lib/view/large_word_view.dart
+++ lib/view/large_word_view.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:todays_kanji/data_source/kanji_source.dart';
+import 'package:todays_kanji/model/kanji_model.dart';
 import 'package:todays_kanji/model/word_model.dart';
+import 'package:todays_kanji/util/general.dart';
+import 'package:todays_kanji/view/word_form_view.dart';
+import 'package:todays_kanji/view/word_sense_view.dart';
 import 'package:todays_kanji/widgets/annotation.dart';
+import 'package:todays_kanji/widgets/content_loader.dart';
 import 'package:todays_kanji/widgets/info_card.dart';
 import 'package:todays_kanji/widgets/japanese_text.dart';
 import 'package:todays_kanji/widgets/large_view_layout.dart';
 
+import 'kanji_view.dart';
+
 class LargeWordView extends StatelessWidget {
   final WordModel model;
+  final kanjiSource = KanjiSource();
 
   LargeWordView(this.model);
 
@@ -31,7 +40,48 @@ class LargeWordView extends StatelessWidget {
       subfocus: JapaneseText(model.forms[0].reading),
       stackAlign: Alignment.topCenter,
       cards: [
-        //TODO add cards
+        Builder(builder: (context) {
+          List<WordSenseView> senses = model.senses
+              .asMap()
+              .entries
+              .map((e) => WordSenseView(e.value, e.key))
+              .toList();
+          return InfoCard(
+            contentIndent: 20,
+            heading: "Meanings",
+            child: Column(children: senses),
+          );
+        }),
+        Builder(builder: (context) {
+          List<WordFormView> forms =
+              model.forms.map((e) => WordFormView(e)).toList()..removeAt(0);
+          return InfoCard(
+            contentIndent: 20,
+            heading: "Other Forms",
+            child: Column(children: forms),
+          );
+        }),
+        Builder(builder: (context) {
+          List<Future<KanjiModel>> futures = model.forms
+              .map((e) => e.word)
+              .join("")
+              .split("")
+              .toSet()
+              .where((char) => isKanji(char))
+              .map((e) => kanjiSource.getKanji(e))
+              .toList();
+          return InfoCard(
+            heading: "Kanji",
+            child: ContentLoader(
+              future: Future.wait(futures),
+              builder: (context, List<KanjiModel> models) {
+                return Column(
+                  children: models.map((e) => KanjiView(e)).toList(),
+                );
+              },
+            ),
+          );
+        })
       ],
     );
   }

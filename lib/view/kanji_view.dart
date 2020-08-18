49f803cc9532ead5aa6eea163f6c17aab07975b1
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-18 21:59:49 +0200

    completed

--- lib/view/kanji_view.dart
+++ lib/view/kanji_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todays_kanji/model/kanji_model.dart';
+import 'package:todays_kanji/widgets/annotation.dart';
 import 'package:todays_kanji/widgets/japanese_text.dart';
 
 class KanjiView extends StatelessWidget {
@@ -11,15 +12,32 @@ class KanjiView extends StatelessWidget {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     var contextWidth = MediaQuery.of(context).size.width;
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        JapaneseText(model.character, style: theme.textTheme.headline3),
-        Container(
-          width: contextWidth * 0.8,
-          child: Text(model.meaning.join("; ")),
-        ),
-      ],
+
+    List<Widget> annotations = [];
+    if (model.frequency > 0)
+      annotations.add(Annotation("${model.frequency}/2500"));
+    if (model.jlpt > 0) annotations.add(JLPTAnnotation(model.jlpt));
+    return InkWell(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          JapaneseText(model.character, style: theme.textTheme.headline3),
+          Container(width: 16),
+          Container(
+            width: contextWidth * 0.5,
+            child: Text(model.meaning.join("; ")),
+          ),
+          Column(
+            children: annotations,
+            crossAxisAlignment: CrossAxisAlignment.end,
+          )
+        ],
+      ),
+      onTap: () => Navigator.pushNamed(
+        context,
+        "/kanji",
+        arguments: model.character,
+      ),
     );
   }
 }

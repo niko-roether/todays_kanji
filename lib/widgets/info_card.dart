6a8a8280757a066292cb0d28c90d01f7d3896265
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-18 16:56:33 +0200

    changed paddings

--- lib/widgets/info_card.dart
+++ lib/widgets/info_card.dart
@@ -31,7 +31,7 @@ class InfoCard extends StatelessWidget {
     if (child != null) {
       content.add(
         Padding(
-          padding: EdgeInsets.fromLTRB(contentIndent, 8, contentIndent, 8),
+          padding: EdgeInsets.fromLTRB(contentIndent, 2, contentIndent, 16),
           child: child,
         ),
       );

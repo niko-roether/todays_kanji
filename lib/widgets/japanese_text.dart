1f40087cf7fc307966f8c4af9967d4ec4cd6a929
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-23 23:37:11 +0200

    added textAlign

--- lib/widgets/japanese_text.dart
+++ lib/widgets/japanese_text.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 class JapaneseText extends StatelessWidget {
   final String text;
   final TextStyle style;
+  final TextAlign textAlign;
 
-  JapaneseText(this.text, {this.style});
+  JapaneseText(this.text, {this.style, this.textAlign});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      textAlign: textAlign,
       style:
           style?.apply(fontFamily: "Meiryo") ?? TextStyle(fontFamily: "Meiryo"),
     );

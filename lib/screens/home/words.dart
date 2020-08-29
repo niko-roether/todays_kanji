bcbe4a837af7b969e73136eef0ce8ef6dda0e772
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-29 20:16:14 +0200

    added loading indicator

--- lib/screens/home/words.dart
+++ lib/screens/home/words.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todays_kanji/app_state.dart';
 import 'package:todays_kanji/widgets/dynamic_lists/word_list.dart';
+import 'package:todays_kanji/widgets/loading_indicator.dart';
 
 class WordsTab extends StatefulWidget {
   @override
@@ -14,6 +15,7 @@ class _WordsTabState extends State<WordsTab>
   Widget build(BuildContext context) {
     super.build(context);
     return Consumer<AppState>(builder: (context, state, child) {
+      if (state.loadingKanji) return LoadingIndicator();
       return WordList(query: "*${state.preferences.kanjiSymbol}*");
     });
   }

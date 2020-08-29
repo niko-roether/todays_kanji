47356909babc1f1281776481cbc9e6253b7d0321
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-30 00:25:05 +0200

    adapted word search

--- lib/widgets/search_buttons/word_search_button.dart
+++ lib/widgets/search_buttons/word_search_button.dart
@@ -3,8 +3,12 @@ import 'package:todays_kanji/delegates/search/wordsearch.dart';
 import 'package:todays_kanji/widgets/search_button.dart';
 
 class WordSearchButton extends StatelessWidget {
+  final wordSearch = WordSearch();
+
   @override
   Widget build(BuildContext context) {
-    return SearchButton(searchDelegate: WordSearch());
+    return SearchButton(
+      searchDelegate: wordSearch,
+    );
   }
 }

347d04316861580e30240d9a956ff5e6ee263a7b
Ph4ntom3 <nicholas.roether@t-online.de>
2020-08-29 23:50:32 +0200

    created

---
+++ lib/delegates/search/wordsearch.dart
@@ -0,0 +1,48 @@
+import 'package:todays_kanji/data_source/kanji_source.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:todays_kanji/delegates/search.dart';
+import 'package:todays_kanji/model/word_form_model.dart';
+import 'package:todays_kanji/model/word_model.dart';
+import 'package:todays_kanji/model/word_sense_model.dart';
+import 'package:todays_kanji/util/general.dart';
+import 'package:todays_kanji/widgets/dynamic_lists/word_list.dart';
+import 'package:todays_kanji/widgets/loading_indicator.dart';
+
+class WordSearch extends Search {
+  final kanjiSource = KanjiSource();
+
+  WordSearch({String initial = ""}) : super(initial: initial);
+
+  List<String> _getSuggestableTerms(WordModel model) {
+    List<String> terms = [];
+    for (WordFormModel form in model.forms) {
+      if (form.reading != null) terms.add(form.reading);
+      if (form.word != null) terms.add(form.word);
+      if (form.readingRomaji != null) terms.add(form.readingRomaji);
+    }
+    for (WordSenseModel sense in model.senses) terms.addAll(sense.definitions);
+    return removeDuplicates(terms);
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return SizedBox.expand(child: WordList(query: query));
+  }
+
+  @override
+  FutureOr<List<String>> fetchSuggestions() async {
+    List<WordModel> models = await kanjiSource.searchWords(query);
+    List<String> suggestions = [];
+    for (WordModel model in models)
+      suggestions.addAll(_getSuggestableTerms(model)
+          .where((element) => element.startsWith(query)));
+    return removeDuplicates(suggestions);
+  }
+
+  @override
+  void addRecentSearch(String query) {
+    // TODO: implement addRecentSearch
+  }
+}
